bd3e0909869c3360d871e631f0df13d7b9492b10
liucong <liucong@huayun.com>
2019-07-10 21:46:46 +0800

    fix huffman

--- binarytree/huffmantree.go
+++ binarytree/huffmantree.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// BinaryTree , when parent is nil,it is empty
 type BinaryTree struct {
 	right  *BinaryTree
 	data   int
@@ -17,6 +18,13 @@ func Println(data int) {
 
 func CreateHuffmanTree() *BinaryTree {
 	return &BinaryTree{}
+}
+
+func (tree *BinaryTree) IsEmpty() bool {
+	if tree.parent == nil {
+		return true
+	}
+	return false
 }
 
 func (tree *BinaryTree) InsertNode(data int) {
@@ -41,8 +49,94 @@ func (tree *BinaryTree) InsertNode(data int) {
 	}
 }
 
-func (tree *BinaryTree) PreOrder(callback func(int)) {
+// FindNode if we find return data,else return -1
+func (tree *BinaryTree) FindNode(data int) *BinaryTree {
 	if tree == nil {
+		return nil
+	}
+	if data == tree.data {
+		return tree
+	}
+
+	if data > tree.data {
+		return tree.right.FindNode(data)
+	}
+
+	if data < tree.data {
+		return tree.left.FindNode(data)
+	}
+	return nil
+}
+
+// RemoveData remove node data math
+func (tree *BinaryTree) RemoveData(data int) {
+	found := tree.FindNode(data)
+	if found == nil {
+		return
+	}
+	if found.left == nil && found.right == nil {
+		tree.RemoveLeafNode(found)
+	} else if found.left == nil {
+		found.data = found.right.data
+		found.left = found.right.left
+		found.right = found.right.right
+		if found.left != nil {
+			found.left.parent = found
+		}
+		if found.right != nil {
+			found.right.parent = found
+		}
+	} else if found.right == nil {
+		found.data = found.left.data
+		found.right = found.left.right
+		found.left = found.left.left
+		if found.left != nil {
+			found.left.parent = found
+		}
+		if found.right != nil {
+			found.right.parent = found
+		}
+	} else {
+		max := found.left.FindMaxNode()
+		if max != nil {
+			found.data = max.data
+			tree.RemoveLeafNode(max)
+		}
+	}
+
+}
+
+// FindMaxNode find max node
+func (tree *BinaryTree) FindMaxNode() *BinaryTree {
+	if tree.left == nil && tree.right == nil {
+		return tree
+	}
+	if tree.right == nil {
+		return tree
+	}
+	return tree.right.FindMaxNode()
+
+}
+
+// RemoveLeafNode remove leaf node
+func (tree *BinaryTree) RemoveLeafNode(node *BinaryTree) {
+	// remove the root node
+	if node.parent == node {
+		tree.parent = nil
+		return
+	}
+	parent := node.parent
+	if parent.right == node {
+		parent.right = nil
+	}
+	if parent.left == node {
+		parent.left = nil
+	}
+
+}
+
+func (tree *BinaryTree) PreOrder(callback func(int)) {
+	if tree == nil || tree.parent == nil {
 		return
 	}
 	callback(tree.data)
@@ -51,7 +145,7 @@ func (tree *BinaryTree) PreOrder(callback func(int)) {
 }
 
 func (tree *BinaryTree) MidOrder(callback func(int)) {
-	if tree == nil {
+	if tree == nil || tree.parent == nil {
 		return
 	}
 	tree.left.MidOrder(callback)
@@ -60,7 +154,7 @@ func (tree *BinaryTree) MidOrder(callback func(int)) {
 }
 
 func (tree *BinaryTree) PostOrder(callback func(int)) {
-	if tree == nil {
+	if tree == nil || tree.parent == nil {
 		return
 	}
 	tree.left.PostOrder(callback)
--- binarytree/huffmantree_test.go
+++ binarytree/huffmantree_test.go
@@ -2,8 +2,9 @@ package huffmantree
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_CreateHuffmanTree(t *testing.T) {
@@ -16,6 +17,7 @@ func Test_InsertNode(t *testing.T) {
 	tree.InsertNode(6)
 	tree.InsertNode(3)
 	tree.InsertNode(2)
+	tree.InsertNode(7)
 	tree.InsertNode(7)
 	tree.InsertNode(5)
 	tree.InsertNode(8)
@@ -33,4 +35,75 @@ func Test_InsertNode(t *testing.T) {
 	fmt.Println()
 
 	assert.NotEqual(t, nil, tree)
+}
+
+func Test_FindNode(t *testing.T) {
+	tree := CreateHuffmanTree()
+	tree.InsertNode(6)
+	tree.InsertNode(3)
+	tree.InsertNode(2)
+	tree.InsertNode(7)
+	tree.InsertNode(7)
+	tree.InsertNode(5)
+	tree.InsertNode(8)
+
+	found := tree.FindNode(4)
+	fmt.Println(found)
+
+	found = tree.FindNode(7)
+	fmt.Println(found.data)
+	assert.Equal(t, found.data, 7)
+
+	found = tree.FindNode(8)
+	fmt.Println(found.data)
+	assert.Equal(t, found.data, 8)
+
+	found = tree.FindNode(6)
+	fmt.Println(found.data)
+	assert.Equal(t, found.data, 6)
+
+	max := tree.FindMaxNode()
+	fmt.Println("find max node :", max.data)
+	assert.Equal(t, max.data, 8)
+
+	tree.RemoveData(7)
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
+
+	tree.RemoveData(2)
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
+
+	tree.RemoveData(8)
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
+
+	tree.RemoveData(5)
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
+
+	tree.RemoveData(6)
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
+
+	tree.RemoveData(7)
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
+
+	tree.RemoveData(3)
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
+
+	tree.RemoveData(8)
+	fmt.Println("after remove.")
+	fmt.Print("midorder:")
+	tree.MidOrder(Println)
+	fmt.Println()
 }
